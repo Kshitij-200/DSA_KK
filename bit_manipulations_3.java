a3f976abd7daafc70730dea330d8f6ae5713019b
Kshitij Pratap Tomer <kshitijpratap2004@gmail.com>
2023-01-23 16:04:38 +0530

    Added extra questions day2

---
+++ bit_manipulations_3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class bit_manipulations_3 {
+    public static int findBit(int n){
+        int i = 0;
+        return 0;
+    }
+    public static void main(String[] args) {
+        int n = 1010010;
+
+    }
+}
