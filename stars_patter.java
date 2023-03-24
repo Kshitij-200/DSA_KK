25d79fb18030b721db140206fb0e1f0e8ec9039b
Kshitij Pratap Tomer <kshitijpratap2004@gmail.com>
2023-03-24 16:14:31 +0530

    Added star pattern

---
+++ stars_patter.java
@@ -0,0 +1,48 @@
+package kshitij;
+
+public class stars_patter {
+//    static void patter1(int n){
+//        for (int row = 1; row<=n;row++){
+//            for (int col = 1; col<=row;col++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+//    static void pattern2(int n){
+//        for (int row = 1;row<=n;row++){
+//            for (int col = 1; col<=n;col++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//    static void pattern3(int n){
+//        for (int row = 5; row >= 1;row--){
+//            for (int col = 4; col >= 5-row;col--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//    static void pattern4(int n){
+//        for (int row = 1; row<=5;row++){
+//            for (int col = 1;col<=row;col++){
+//                System.out.print(col);
+//            }
+//            System.out.println();
+//        }
+//    }
+    static void pattern5(int n){
+
+    }
+    public static void main(String[] args) {
+//        patter1(5);
+//        pattern2(5);
+//        pattern3(5);
+//        pattern4(5);
+        pattern5(5);
+
+    }
+}
